9b1d7e2d5b482d960fcd1a06d7e74998a7d7051e
Daniel Teigland <daniel.teigland.developer@gmail.com>
2022-09-01 20:56:32 -0400

    added create player

--- first_api/api/basketball/routes.py
+++ first_api/api/basketball/routes.py
@@ -7,3 +7,8 @@ router = APIRouter()
 @router.get("/players")
 def get_players():
     return {"players": ["Lebron James", "Kevin Durant", "Stephen Curry"]}
+
+
+@router.post("/players")
+def add_player(player: str):
+    return {"player": player}
