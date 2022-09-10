4039ce67fe9e3143bde0066d5396b4c41952e617
Daniel Teigland <daniel.teigland.developer@gmail.com>
2022-09-10 15:26:21 -0400

    allow answer of questions in DB

--- first_api/api/survey/routes.py
+++ first_api/api/survey/routes.py
@@ -1,7 +1,8 @@
-from typing import List
+from datetime import datetime
+from typing import Dict, List
 from fastapi import APIRouter, HTTPException
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from first_api.db import ClassInDB
 
@@ -26,6 +27,24 @@ class CreateQuestionForm(BaseModel):
     answers: List[Answer]
 
 
+class AnswerInDB(ClassInDB):
+    name: str
+    answer_date: datetime = Field(default_factory=datetime.now)
+    question_id: str
+    answer_id: str
+
+
+class AnswerQuestionsForm(BaseModel):
+    name: str
+    answers: Dict[str, str]
+
+
+class SurveyEvaluationResult(BaseModel):
+    name: str
+    score: float
+    answers: List[Answer]
+
+
 @router.post("/question/create", response_model=QuestionInDB)
 def create_question(form_data: CreateQuestionForm):
     question = QuestionInDB(
@@ -44,3 +63,26 @@ def create_question(form_data: CreateQuestionForm):
 @router.get("/", response_model=List[QuestionInDB])
 def get_questions():
     return QuestionInDB.read_all()
+
+
+@router.post("/answer")
+def answer_survey(
+    form_data: AnswerQuestionsForm,
+):
+    real_answers = []
+    for question_id, answer_id in form_data.answers.items():
+        question = QuestionInDB.read(question_id)
+        real_answers.append(next((a for a in question.answers if a.id == answer_id)))
+
+        answer = AnswerInDB(
+            id=f"{form_data.name}-{question_id}-{datetime.now()}",
+            name=form_data.name,
+            question_id=question_id,
+            answer_id=answer_id,
+        )
+        answer.save(overwrite=True)
+    return SurveyEvaluationResult(
+        name=form_data.name,
+        score=sum(a.value for a in real_answers),
+        answers=real_answers,
+    )
